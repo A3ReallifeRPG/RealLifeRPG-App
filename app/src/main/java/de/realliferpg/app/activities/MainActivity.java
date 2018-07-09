473c8b10f32737e644b5f66c17b77850ff46379b
vabene1111 <vabene1234@googlemail.com>
2018-07-09 21:56:00 +0200

    basic fragments working

--- app/src/main/java/de/realliferpg/app/activities/MainActivity.java
+++ app/src/main/java/de/realliferpg/app/activities/MainActivity.java
@@ -1,12 +1,8 @@
-package de.realliferpg.app;
+package de.realliferpg.app.activities;
 
-import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -16,8 +12,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import de.realliferpg.app.R;
+import de.realliferpg.app.fragments.ChangelogFragment;
+import de.realliferpg.app.fragments.ImprintFragment;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,ImprintFragment.OnFragmentInteractionListener , ChangelogFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,7 +84,14 @@ public class MainActivity extends AppCompatActivity
 
             transaction.commit();
         } else if (id == R.id.nav_gallery) {
+            ChangelogFragment changelogFragment = new ChangelogFragment();
 
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            transaction.replace(R.id.include_main_content, changelogFragment);
+            transaction.addToBackStack(null);
+
+            transaction.commit();
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
@@ -100,4 +107,8 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
