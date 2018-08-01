a376c9ff82a4a08692307a833d83ad22529bb42a
vabene1111 <vabene1234@googlemail.com>
2018-08-01 22:04:58 +0200

    working with lib

--- app/src/main/java/de/realliferpg/app/fragments/SettingsFragment.java
+++ app/src/main/java/de/realliferpg/app/fragments/SettingsFragment.java
@@ -1,59 +1,18 @@
 package de.realliferpg.app.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.support.v7.preference.PreferenceFragmentCompat;
+import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
 
 import de.realliferpg.app.R;
-import de.realliferpg.app.interfaces.FragmentInteractionInterface;
-
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
-    View view;
-    private FragmentInteractionInterface mListener;
-
-    public SettingsFragment() {
-
-    }
-
-
-    public static SettingsFragment newInstance() {
-        return new SettingsFragment();
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+    public void onCreatePreferencesFix(@Nullable Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+
+        // additional setup
     }
-
-    @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-
-    }
-
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof FragmentInteractionInterface) {
-            mListener = (FragmentInteractionInterface) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
 }
