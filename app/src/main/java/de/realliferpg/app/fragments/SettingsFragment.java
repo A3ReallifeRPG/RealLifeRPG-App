8afc84c60e664767f2ec5c9a5943c7a9d09a538a
vabene1111 <vabene1234@googlemail.com>
2018-08-02 09:54:32 +0200

    working base

--- app/src/main/java/de/realliferpg/app/fragments/SettingsFragment.java
+++ app/src/main/java/de/realliferpg/app/fragments/SettingsFragment.java
@@ -1,16 +1,32 @@
 package de.realliferpg.app.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceManager;
 
-import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
+
 
 import de.realliferpg.app.R;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    public static final String KEY_PREF_API_TOKEN = "pref_api_token";
+
 
     @Override
-    public void onCreatePreferencesFix(@Nullable Bundle savedInstanceState, String rootKey) {
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+        PreferenceManager.setDefaultValues(this.getContext(), R.xml.preferences, false);
     }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(KEY_PREF_API_TOKEN)) {
+            // stuff
+        }
+    }
+
+
 }
