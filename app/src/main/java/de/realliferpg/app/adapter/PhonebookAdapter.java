cf3311d3ba205495c97d7a164c15c0ebf886866a
Zenty96 <z3nty96@gmail.com>
2021-04-16 16:26:13 +0200

    Telefonbuch eingebaut
    Aufruf (ohne Daten) eingebaut

---
+++ app/src/main/java/de/realliferpg/app/adapter/PhonebookAdapter.java
@@ -0,0 +1,143 @@
+package de.realliferpg.app.adapter;
+
+import android.content.Context;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import de.realliferpg.app.R;
+import de.realliferpg.app.objects.Server;
+
+
+public class PhonebookAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private ArrayList<Server> servers;
+
+    public PhonebookAdapter(Context _context, ArrayList<Server> _servers) {
+        this.context = _context;
+        this.servers = _servers;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return servers.size();
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return servers.get(i).Players.length;
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return servers.get(i);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        String[] sortedPlayers = servers.get(groupPosition).Players;
+        Arrays.sort(sortedPlayers);
+        return sortedPlayers[childPosition];
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        Server server = (Server) getGroup(groupPosition);
+
+        final ViewHolder viewHolder;
+
+        if(convertView == null)
+        {
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item_group_playerslist,null);
+
+            viewHolder = new ViewHolder();
+            viewHolder.position = groupPosition;
+
+            viewHolder.tvPlayersGroupInfo = convertView.findViewById(R.id.tv_playerslist_info);
+            viewHolder.tvServerHead = convertView.findViewById(R.id.tv_playerslist_grouphead);
+            viewHolder.pbCountPlayers = convertView.findViewById(R.id.pb_playerslist_count);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.position = groupPosition;
+
+        String title = server.Servername + " - " + server.Playercount + "/" + server.Slots;
+        viewHolder.tvServerHead.setText(title);
+
+        double progress = ((double) server.Playercount / (double) server.Slots) * 100;
+
+        viewHolder.pbCountPlayers.setProgress((int) progress);
+
+        if (server.Servername.toLowerCase().contains("realliferpg") && !server.Servername.toLowerCase().contains("gungame")) {
+            // Arma 3 Server
+            viewHolder.tvPlayersGroupInfo.setText(Html.fromHtml(
+                    "<font color='" + convertView.getResources().getColor(R.color.colorCiv) + "'>CIV " + server.Civilians +
+                            "</font> - <font color='" + convertView.getResources().getColor(R.color.colorCop) + "'>COP " + server.Cops +
+                            "</font> - <font color='" + convertView.getResources().getColor(R.color.colorMed) + "'>MED " + server.Medics +
+                            "</font> - <font color='" + convertView.getResources().getColor(R.color.colorRac) + "'>RAC " + server.Adac +
+                            "</font>"));
+        }
+        else {
+            // anderer Server
+            viewHolder.tvPlayersGroupInfo.setText(Html.fromHtml(
+                    "<font color='" + convertView.getResources().getColor(R.color.colorCiv) + "'>Spieler " + server.Playercount));
+        }
+
+        convertView.setTag(viewHolder);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final String childText = (String)getChild(groupPosition,childPosition);
+
+        if(convertView == null)
+        {
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item_playerslist,null);
+        }
+
+        TextView text_playerslist_listitem = convertView.findViewById(R.id.tv_playerslist_listitem);
+        text_playerslist_listitem.setText(Html.fromHtml(childText));
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    static class ViewHolder {
+        TextView tvServerHead;
+        TextView tvPlayersGroupInfo;
+        ProgressBar pbCountPlayers;
+        int position;
+    }
+}
---
+++ app/src/main/java/de/realliferpg/app/fragments/PhonebookFragment.java
@@ -0,0 +1,160 @@
+package de.realliferpg.app.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+
+import de.realliferpg.app.Constants;
+import de.realliferpg.app.R;
+import de.realliferpg.app.Singleton;
+import de.realliferpg.app.adapter.PhonebookAdapter;
+import de.realliferpg.app.helper.ApiHelper;
+import de.realliferpg.app.interfaces.CallbackNotifyInterface;
+import de.realliferpg.app.interfaces.FragmentInteractionInterface;
+import de.realliferpg.app.interfaces.RequestCallbackInterface;
+import de.realliferpg.app.interfaces.RequestTypeEnum;
+import de.realliferpg.app.objects.CustomNetworkError;
+import de.realliferpg.app.objects.Server;
+
+public class PhonebookFragment extends Fragment implements CallbackNotifyInterface {
+
+    private View view;
+    private FragmentInteractionInterface mListener;
+
+    public PhonebookFragment() {
+        // Required empty public constructor
+    }
+
+    public static PhonebookFragment newInstance() {
+        return new PhonebookFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view =  inflater.inflate(R.layout.fragment_phonebooks, container, false);
+
+        final ProgressBar pbLoadphonebook = view.findViewById(R.id.pb_phonebook);
+        pbLoadphonebook.setVisibility(View.VISIBLE);
+        final ExpandableListView listPlayers = view.findViewById(R.id.lv_phonebook);
+
+        final ApiHelper apiHelper = new ApiHelper((RequestCallbackInterface) getActivity());
+        if (Singleton.getInstance().getPlayerInfo() == null) {
+            apiHelper.getPlayerStats();
+        }
+
+        final SwipeRefreshLayout sc = view.findViewById(R.id.srl_main_phonebook);
+        sc.setColorSchemeColors(view.getResources().getColor(R.color.primaryColor));
+        sc.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                apiHelper.getPlayerStats();
+
+                pbLoadphonebook.setVisibility(View.VISIBLE);
+
+                listPlayers.setAdapter((BaseExpandableListAdapter) null);
+            }
+        });
+
+        listPlayers.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                int topRow = (listPlayers == null || listPlayers.getChildCount() == 0) ?
+                        0 : listPlayers.getChildAt(0).getTop();
+                sc.setEnabled(firstVisibleItem == 0 && topRow >= 0);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof FragmentInteractionInterface) {
+            mListener = (FragmentInteractionInterface) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onCallback(RequestTypeEnum type) {
+        ProgressBar pbListPlayers = view.findViewById(R.id.pb_phonebook);
+        SwipeRefreshLayout sc = view.findViewById(R.id.srl_main_phonebook);
+        sc.setRefreshing(false);
+
+        switch (type) {
+            case SERVER:
+                ArrayList<Server> servers = Singleton.getInstance().getServerList();
+                final ExpandableListView listPhoneBooks = view.findViewById(R.id.lv_phonebook);
+
+                PhonebookAdapter listAdapter = new PhonebookAdapter(this.getContext(), servers);
+
+                listPhoneBooks.setAdapter(listAdapter);
+
+                pbListPlayers.setVisibility(View.GONE);
+
+                // collapse all but selected item
+                listPhoneBooks.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+                    int previousItem = -1;
+
+                    @Override
+                    public void onGroupExpand(int groupPosition) {
+                        if (groupPosition != previousItem)
+                            listPhoneBooks.collapseGroup(previousItem);
+                        previousItem = groupPosition;
+                    }
+                });
+                break;
+            case NETWORK_ERROR:
+                CustomNetworkError error = Singleton.getInstance().getNetworkError();
+
+                pbListPlayers.setVisibility(View.GONE);
+
+                Singleton.getInstance().setErrorMsg(error.toString());
+
+                Snackbar snackbar = Snackbar.make(view.findViewById(R.id.cl_main_phonebook), R.string.str_error_occurred, Constants.ERROR_SNACKBAR_DURATION);
+
+                snackbar.setAction(R.string.str_view, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mListener.onFragmentInteraction(ChangelogFragment.class, Uri.parse("open_error"));
+                    }
+                });
+
+                snackbar.show();
+                Singleton.getInstance().setCurrentSnackbar(snackbar);
+                break;
+        }
+    }
+}
